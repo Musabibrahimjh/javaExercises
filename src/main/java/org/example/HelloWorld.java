85e0aefdd3f4ccb9f5c0decdb53d2d45d3c052f1
Musabibrahimjh <139059901+Musabibrahimjh@users.noreply.github.com>
2024-05-13 14:53:48 +0300

    My second java commit

--- src/main/java/org/example/HelloWorld.java
+++ src/main/java/org/example/HelloWorld.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Welcome to the playground!");
+        System.out.println("Welcome to the play ground!");
     }
 }
