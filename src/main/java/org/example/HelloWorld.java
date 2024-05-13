ba0f7d807d5ef89bb82af547f9421f7890d80c6d
Musabibrahimjh <139059901+Musabibrahimjh@users.noreply.github.com>
2024-05-13 14:55:36 +0300

    Update HelloWorld.java

--- src/main/java/org/example/HelloWorld.java
+++ src/main/java/org/example/HelloWorld.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Welcome to the play ground!");
+        System.out.println("Welcome to the playground!");
     }
 }
